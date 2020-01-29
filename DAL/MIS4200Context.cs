c73fc36c814cdf40ca93cc9fecb71271259e5d85
Kathleen Lovgren <kl261315@ohio.edu>
2020-01-29 10:32:57 -0500

    DAL, Controllers, Class customer and orders

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LovgrenMIS4200.Models;
+
+namespace LovgrenMIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
